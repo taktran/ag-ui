5e435e71b9bb1e51946be1b1d434e4ec730159c3
Tak Tran <contact@tutaktran.com>
2024-04-05 17:06:13 +0100

    Change enterprise to lightbulb

--- src/icon/EnterpriseIcon.tsx
+++ src/icon/EnterpriseIcon.tsx
@@ -7,7 +7,7 @@ export const EnterpriseIcon: FunctionComponent = () => {
     return (
         <span className={styles.enterpriseIcon}>
             (e)
-            <Icon name="enterprise" />
+            <Icon name="lightBulb" />
         </span>
     );
 };
